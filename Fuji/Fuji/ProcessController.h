8a7f11f7da7914048476a54474c98f21e8b880f0
Laura Aubin <laura.aubin@shopify.com>
2018-03-23 21:03:41 -0400

    Remove comments

--- Fuji/Fuji/ProcessController.h
+++ Fuji/Fuji/ProcessController.h
@@ -10,19 +10,9 @@
 
 #import <Cocoa/Cocoa.h>
 
-// for defining what kind of OS is going to be able to run our application
-@interface operatingSystemArchitecture : NSObject
-@end
-
 @interface ProcessController : NSObject
-   // weak: keeps a pointer to the object so long as it exists. When it is deallocated, pointer is set to nil automatically.
-   // readonly: only generate a getter, no setter. Will warn you if you try to change this property.
-   // NSWorkspace: can launch apps and handle file services. There is one shared workspace within the entire app.
    @property (weak, readonly) NSWorkspace *workspace;
-
-   // Sorts an array as part of arrangeObjects.
    @property(weak, readonly) NSArray *sortDescriptors;
-
    @property (strong) IBOutlet NSArrayController *arrayOfRunningProcesses;
 @end
 
--- Fuji/Fuji/ProcessController.m
+++ Fuji/Fuji/ProcessController.m
@@ -11,19 +11,14 @@
 
 @implementation ProcessController
 
-    // Tells the delegate that the app has become active.
     - (void)applicationDidBecomeActive:(NSNotification *)notification
     {
-        // rearrageObjects is a method that calls arrangeObjects which returns an array containing filtered and sorted (sortDescriptors) objects.
         [self.arrayOfRunningProcesses rearrangeObjects];
-        
-        NSLog(@"Array: %@", _arrayOfRunningProcesses); // printing
+        NSLog(@"Array: %@", _arrayOfRunningProcesses);
     }
 
-    // Workspaces are used to launch apps and perform file services.
     - (NSWorkspace *)workspace;
     {
-        // You can call sharedWorkspace from anywhere in the app, so it is needed to bind to the UI.
         return [NSWorkspace sharedWorkspace];
     }
 @end
