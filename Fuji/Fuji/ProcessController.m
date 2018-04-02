17c460f2b55565d699b2178056e74a913a702faf
Laura Aubin <laura.aubin@shopify.com>
2018-04-02 01:10:54 -0400

    Include the logic for reading CPU usage

--- Fuji/Fuji/ProcessController.m
+++ Fuji/Fuji/ProcessController.m
@@ -8,6 +8,9 @@
 #import <Foundation/Foundation.h>
 #import "ProcessController.h"
 #import <Cocoa/Cocoa.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 extern int CurrentlySelectedProcessID; // access the global variable declared in main
 
@@ -45,7 +48,46 @@ extern int CurrentlySelectedProcessID; // access the global variable declared in
 
     - (int) selectedCPU;
     {
-        return 0;
+        FILE *pipeStream;
+        
+        char cpuBuffer[500];
+        int done = 0;
+        
+        char selectedPID[50];
+        sprintf(selectedPID, "%d", CurrentlySelectedProcessID);
+        
+        char command[100] = "ps -p ";
+        strcat(command, selectedPID);
+        strcat(command, " -o %cpu");
+        
+        // open the command for reading
+        // -n1 : only run once
+        pipeStream = popen(command, "r");
+        if (pipeStream == NULL) {
+            printf("Failed to run command\n" );
+            exit(1);
+        }
+        
+        // Read the output one line at a time
+        // 2 is the number of lines we are traversing to find the one that we will parse
+        while ((done < 2) && (fgets(cpuBuffer, sizeof(cpuBuffer) - 1, pipeStream) != NULL)) {
+            done++;
+        }
+        
+        pclose(pipeStream);
+        
+        char cpuUsage[100];
+        int cpuIndex = 0;
+        int cpuBufferIndex = 2;
+        
+        for(int i = 0; i < 3; i++){
+            cpuUsage[cpuIndex++] = cpuBuffer[cpuBufferIndex++];
+        }
+        
+        float cpuUsageFloat;
+        cpuUsageFloat = (float)atof(cpuUsage);
+        
+        return cpuUsageFloat;
     }
 @end
 
