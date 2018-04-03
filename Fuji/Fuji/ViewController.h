5fa6a2b39723798f32a94916906d9a1feb1030ea
Laura Aubin <laura.aubin@shopify.com>
2018-04-03 19:46:37 -0400

    Calculate the total CPU usage being by the system

--- Fuji/Fuji/ViewController.h
+++ Fuji/Fuji/ViewController.h
@@ -20,13 +20,16 @@
     @property (strong) IBOutlet NSTextField *increasingProcessPriorityDisplay;
     @property (strong) IBOutlet NSTextField *decreasingProcessPriorityDisplay;
 
+    // individual process' CPU value
     @property (readonly) NSString *selectedProcessCPU;
     @property (strong) IBOutlet NSTextField *cpuRefreshValue;
-
-    - (int)getCurrentProcessPriority;
-
     - (float)calculateIndividualCPU;
     - (void)updateCpuDisplayText;
+
+    // total system CPU value
+    - (float)calculateTotalCPU;
+
+    - (int)getCurrentProcessPriority;
 @end
 
 #endif /* ViewController_h */
